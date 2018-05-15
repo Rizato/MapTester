8846703e4d39716423a99e05c4545d6e295eb84f
Rob Lathrop <rldifts@gmail.com>
2018-05-15 12:41:03 -0400

    Remove Mio. Add Tokio.

--- src/conn/mod.rs
+++ src/conn/mod.rs
@@ -16,3 +16,52 @@ limitations under the License.*/
 ///This just declares a couple more modules
 pub mod server;
 pub mod api;
+
+use futures::sync::mpsc;
+use std::net::SocketAddr;
+use std::collections::HashMap;
+
+pub type Tx = mpsc::UnboundedSender<Msg>;
+pub type Rx = mpsc::UnboundedReceiver<Msg>;
+
+/// enum for passing messages between connection & game loop. These are handled in the notify
+/// method of the mio Handler
+pub enum Msg {
+    Command(SocketAddr, String),
+    Connect(SocketAddr, Tx),
+    Timeout(SocketAddr),
+    Login(SocketAddr, Login),
+    Image(String),
+    Name(String),
+    Hp(i32),
+    Mana(i32),
+    Gold(i32),
+    Xp(i32),
+    LoginResult(u8, String),
+    Quit,
+    //Screen(MapScreen),
+    Shout(String),
+    TextOutput(u8, String),
+    Tile(i16, String),
+    TileMapping(HashMap<String, i16>),
+}
+
+pub struct Login {
+    pub height: i16,
+    pub width: i16,
+    pub name: String,
+    pub password: String,
+    pub version: String,
+}
+
+impl Login {
+    pub fn new(height: i16, width: i16, name: &str, password: &str, version: &str) -> Self {
+        Login {
+            height,
+            width,
+            name: name.to_owned(),
+            password: password.to_owned(),
+            version: version.to_owned(),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -14,35 +14,65 @@ See the License for the specific language governing permissions and
 limitations under the License.*/
 
 
-pub mod game;
 pub mod conn;
 
-extern crate mio;
+extern crate tokio;
+#[macro_use]
+extern crate futures;
 extern crate flate2;
 extern crate time;
 extern crate xml;
 extern crate glob;
+extern crate bytes;
 
-use conn::server::Server;
-
-use mio::tcp::*;
+use conn::{Tx};
+use conn::api::Codec;
+use conn::server::Player;
+use futures::sync::mpsc;
+use std::sync::{Mutex, Arc};
 use std::net::SocketAddr;
-
+use tokio::net::TcpListener;
+use tokio::prelude::*;
 
 /// This is the source for a MOBA server that is compatible with a preexisting game client. 
 
 fn main() {
     //This section starts up a tcp socket listening on port 2222, per the client docs
     println!("starting");
+
+    let (tx, _rx) = mpsc::unbounded();
+    // I need to figure out how to create the game here before the process_socket. 
+    let game = Arc::new(Mutex::new(SharedState::new(tx)));
+
     let addr: SocketAddr = "0.0.0.0:2222".parse().unwrap();
-    println!("addr");
-    let server = TcpListener::bind(&addr).unwrap();
-    println!("server");
-    let mut event_loop = mio::EventLoop::new().unwrap();
-    println!("event_loop"); 
-    event_loop.register(&server, conn::server::SERVER).unwrap();
-    println!("register");
-    event_loop.timeout_ms(conn::server::TIMEOUT, 1).unwrap();
-    let mut moba = Server::new(server);
-    let _ = event_loop.run(&mut moba).unwrap();
+    let listener = TcpListener::bind(&addr).unwrap();
+
+
+    let server = listener.incoming().for_each(move |socket| {
+        let codec = Codec::new(socket);
+
+        let player = Player::new(game.clone(), codec)
+            .map_err(|e| {
+                println!("Player Error: {:?}", e);
+            });
+        tokio::spawn(player);
+        Ok(())
+    })
+    .map_err(|e| {
+        println!("Conn Error: {:?}", e);
+    });
+
+    tokio::run(server);
+}
+
+pub struct SharedState {
+    game: Tx,
+}
+
+impl SharedState {
+    pub fn new(tx: Tx ) -> Self {
+        SharedState {
+            game: tx,
+        }
+    }
 }
